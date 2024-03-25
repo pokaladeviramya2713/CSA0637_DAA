7a9d902669d418ab9d63fa3d1fa61c656d0560b8
pokaladeviramya2713 <130475902+pokaladeviramya2713@users.noreply.github.com>
2024-03-25 12:24:03 +0530

    Update 1.FIBONACCI RECURRSION.cpp

--- 1.FIBONACCI RECURRSION.cpp
+++ 1.FIBONACCI RECURRSION.cpp
@@ -1,33 +1,17 @@
 #include <stdio.h>
-
-int power(int base, int exponent) {
-    if (exponent == 0)
-        return 1;
-    return base * power(base, exponent - 1);
-}
-
-int countDigits(int num) {
-    if (num == 0)
-        return 0;
-    return 1 + countDigits(num / 10);
-}
-
-int isArmstrong(int num, int originalNum) {
-    if (num == 0)
-        return 0;
-    return power(num % 10, countDigits(originalNum)) + isArmstrong(num / 10, originalNum);
-}
-
-int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (isArmstrong(num, num) == num)
-        printf("%d is an Armstrong number.\n", num);
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
     else
-        printf("%d is not an Armstrong number.\n", num);
-
+        return fibonacci(n-1) + fibonacci(n-2);
+}
+int main() {
+    int n, i;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
     return 0;
 }
-
